4d61f86b17d00916e22056fc0830174e3233a59c
Vaibhav <vmalpani@shutterstock.com>
2016-10-26 15:54:18 -0400

    looping in all the frames

--- src/tracker.py
+++ src/tracker.py
@@ -1,10 +1,12 @@
+import glob
+
 import numpy as np
 import cv2
 
 import helper
 
 
-def generate_histogram(quantized_img, (x, y, w, h), bins=16):
+def generate_histogram(quantized_img, (x, y, w, h), bins=32):
     template = quantized_img[y:y+h, x:x+w]
     car_hist, _ = np.histogram(template.flatten(), bins=bins, range=(0, bins))
     car_hist = car_hist.astype(float) / np.sum(car_hist)
@@ -36,46 +38,42 @@ def expand_search_window(img, rect, search_window_size=120):
 
 
 def main():
-    first_img = cv2.imread('data/00000001.jpg')
+    images = sorted(glob.glob('data/*.jpg'))
+    first_img = cv2.imread(images[0])
     bbox = [6, 166, 43, 27]
     helper.draw_bbox("result/00000001.jpg", first_img, bbox)
 
     # first_img = helper.median_filtering(first_img)
-    quantized_img = first_img // 16
+    quantized_img = first_img // 32
     car_hist = generate_histogram(quantized_img, bbox)
 
-    test_img = cv2.imread('data/00000002.jpg')
-    # test_img = helper.median_filtering(test_img)
-    quantized_img = test_img // 16
-    # test_hist = generate_histogram(quantized_img, (19, 169, 46, 25))
+    for img in images[1:]:
+        test_img = cv2.imread(img)
+        # first_img = helper.median_filtering(first_img)
+        quantized_img = test_img // 32
 
-    # 6.00,193.00,6.00,166.00,49.00,166.00,49.00,193.00
-    # 19.00,194.00,19.00,169.00,65.00,169.00,65.00,194.00
-    # x, y, w, h = 19, 169, 46, 25
+        candidate_windows = []
+        candidate_scores = []
+        max_score = -1
+        best_window = None
+        search_window = expand_search_window(test_img, bbox,
+                                             search_window_size=60)
+        for (x, y, window) in helper.sliding_window_new(test_img,
+                                                        search_window,
+                                                        stride=8,
+                                                        template_size=(bbox[2], bbox[3])):  # noqa
+            tmp_histogram = generate_histogram(quantized_img,
+                                               (x, y, bbox[2], bbox[3]))
+            ncc_score = helper.ncc(tmp_histogram, car_hist)
+            candidate_windows.append((x, y, bbox[2], bbox[3]))
+            candidate_scores.append(ncc_score)
+            if ncc_score > max_score:
+                max_score = ncc_score
+                best_window = (x, y, bbox[2], bbox[3])
+        bbox = best_window
+        helper.draw_bbox("result/" + img.split('/')[-1], test_img, best_window)
 
-    candidate_windows = []
-    candidate_scores = []
-    max_score = -1
-    best_window = None
-    best_crop = None
-    cv2.imshow("test", test_img)
-    cv2.waitKey(5)
-    search_window = expand_search_window(test_img, bbox,
-                                         search_window_size=120)
-    for (x, y, window) in helper.sliding_window_new(test_img, search_window,
-                                                    stride=8,
-                                                    template_size=(bbox[2], bbox[3])):  # noqa
-        tmp_histogram = generate_histogram(quantized_img,
-                                           (x, y, bbox[2], bbox[3]))
-        ncc_score = helper.chi_squared(tmp_histogram, car_hist)
-        candidate_windows.append((x, y, bbox[2], bbox[3]))
-        candidate_scores.append(ncc_score)
-        if ncc_score > max_score:
-            max_score = ncc_score
-            best_window = (x, y, bbox[2], bbox[3])
-            best_crop = window
-
-        """
+    """
         # uncomment to visualize prev bbox, expanded search box, sliding window
         clone = test_img.copy()
         cv2.rectangle(clone, (bbox[0], bbox[1]),
@@ -87,13 +85,13 @@ def main():
                              (0, 0, 255), 2)
         cv2.rectangle(clone, (x, y), (x + bbox[2], y + bbox[3]),
                                      (0, 255, 0), 2)
-        cv2.imshow("window", clone)
+        cv2.imshow("sliding window visualizer", clone)
         cv2.waitKey(1)
         import time
         time.sleep(0.25)
-        """
     helper.draw_bbox("result/0000002.jpg", test_img, best_window)
     cv2.imwrite("result/0000002_crop.jpg", best_crop)
+    """
 
 if __name__ == "__main__":
     main()
